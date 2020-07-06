b4c233c29cac97702940ee20d9cce02e95eb45c2
Duncan Watson-Parris <duncan@watson-parris.co.uk>
2020-07-06 20:43:29 +0100

    Major tidy-up and refactor. Not tested.

--- data_analysis/contours_one_loaded_amsre.py
+++ data_analysis/contours_one_loaded_amsre.py
@@ -1,53 +1,39 @@
 import xarray as xr
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import pickle
-import dask.array as da
 import cv2
-import math
 from multiprocessing import Pool
 from glob import glob
 import itertools
 import os
 
-fileload = 1
-num_procs = 8
-#dataDIR = 'Archive/*hdf_poc_5km.nc'
-#dataDIR = '/gws/nopw/j04/eo_shared_data_vol2/scratch/dwatsonparris/AMSR2_*rain.nc'
-dataDIR = '/gws/nopw/j04/eo_shared_data_vol2/scratch/dwatsonparris/AMSRE_*_????_rain.nc'
-filelist = glob(dataDIR)
-print(len(filelist))
-print(filelist)
-
-def oneDArray(x):
-    return list(itertools.chain(*x))
 
 def image_data_operation(data, mask, method='Mean'):
     if method == 'Mean':
         return np.nanmean(data*mask)
     elif method == 'MinMax':
-        return cv2.minMaxLoc(data.astype('float32'), mask = mask.astype('uint8'))
-    
+        return cv2.minMaxLoc(data.astype('float32'), mask=mask.astype('uint8'))
+
+
 def striped_boundary_data(mask, stripe_width, no_stripes, data, method='Mean'):
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (stripe_width*2, stripe_width*2))
-    masks = np.zeros((no_stripes,mask.shape[0],mask.shape[1]))
+    masks = np.zeros((no_stripes, mask.shape[0], mask.shape[1]))
     inner_mask = mask
-    outer_mask = np.zeros((mask.shape[0],mask.shape[1]))
+    outer_mask = np.zeros((mask.shape[0], mask.shape[1]))
     #Create the inner stripes
     for i in range(int(math.floor(no_stripes/2))):
-        inner_mask = cv2.erode(inner_mask.astype('float32'), kernel, iterations = 1)
-        masks[math.floor(no_stripes/2-1)-i,...] = mask.astype('float32')-inner_mask-outer_mask
-        outer_mask = outer_mask + masks[math.floor(no_stripes/2-1)-i,...]
+        inner_mask = cv2.erode(inner_mask.astype('float32'), kernel, iterations=1)
+        masks[math.floor(no_stripes/2-1)-i, ...] = mask.astype('float32')-inner_mask-outer_mask
+        outer_mask = outer_mask + masks[math.floor(no_stripes/2-1)-i, ...]
     #Create the outer stripes
     outer_mask = mask.astype('float32')
     inner_mask = mask.astype('float32')
     for i in range(int(math.ceil(no_stripes/2))):
-        outer_mask = cv2.dilate(outer_mask, kernel, iterations = 1)
-        masks[math.floor(no_stripes/2)+i,...] = outer_mask - inner_mask
+        outer_mask = cv2.dilate(outer_mask, kernel, iterations=1)
+        masks[math.floor(no_stripes/2)+i, ...] = outer_mask - inner_mask
         inner_mask = outer_mask
     #Masks OK
-    masks[masks==0] = np.nan
+    masks[masks == 0] = np.nan
 
     graphs = {}
     for variable, array in data.items():
@@ -57,15 +43,11 @@ def striped_boundary_data(mask, stripe_width, no_stripes, data, method='Mean'):
             if np.amax(masks[i,...]) == 0:
                 graphs[variable][i] = np.nan
             else:
-                graphs[variable][i] = image_data_operation(array, masks[i,...], method=method)
+                graphs[variable][i] = image_data_operation(array, masks[i, ...], method=method)
     return graphs
 
-def datagen(n, filelist):
-    for i in range(math.ceil(len(filelist)/n)):
-        print('Batch', i)
-        yield filelist[i*n:(i+1)*n]
 
-def data_extract(file):
+def data_extract(file, stripe_width=1, n_stripes=40):
     try:
         DS = xr.open_mfdataset(file)
     except:
@@ -75,93 +57,16 @@ def data_extract(file):
     if not hasattr(DS, 'poc_mask'):
         print("file doesn't contain a mask: " + str(file))
         return None
-        
-    #if DS['poc_mask'].values.any() == 0:
-    #    return None
-    data = {}
-    #data comes in as one long vector, reshape to modis image size, first dimension is image index
-    #    rain_time = 263 ;
-    #    rain_swath_width = 486 ;
-    #    lwp_time = 263 ;
-    #    lwp_swath_width = 243 ;
 
-    #   rain_time = 264 ;
-    #   rain_swath_width = 486 ;
-    #   yr_day_utc = 3 ;
-    #   lwp_time = 263 ;
-    #   lwp_swath_width = 243 ;
+    label_no, labels = cv2.connectedComponents((DS['poc_mask']>127).astype('uint8'))
 
-    #print(DS.poc_mask.shape[0], shape)
-    # AMSRE rain
-    #if DS.poc_mask.shape[0] % (265*486)== 0:
-    #    shape = (int(DS.poc_mask.shape[0]/(265*486)),486,265)
-    #elif DS.poc_mask.shape[0] % (264*486) == 0:
-    #    shape = (int(DS.poc_mask.shape[0]/(264*486)),486,264)
-    #elif DS.poc_mask.shape[0] % (263*486) == 0:
-    #    shape = (int(DS.poc_mask.shape[0]/(263*486)),486,263)
-    # AMSRE LWP
-    #elif DS.poc_mask.shape[0] % (265*243) == 0:
-    #    shape = (int(DS.poc_mask.shape[0]/(265*243)),243,265)
-    # AMSR2
-#        rain_time = 528 ;
-#        rain_swath_width = 486 ;
-#        lwp_time = 264 ;
-#        lwp_swath_width = 243 ;
-    #elif DS.poc_mask.shape[0] % (529*486) == 0:
-    #    shape = (int(DS.poc_mask.shape[0]/(529*486)),529,486)
-    #elif DS.poc_mask.shape[0] % (528*486) == 0:
-    #    shape = (int(DS.poc_mask.shape[0]/(528*486)),528,486)
-    #elif DS.poc_mask.shape[0] % (527*486) == 0:
-    #    shape = (int(DS.poc_mask.shape[0]/(527*486)),527,486)
-    #elif DS.poc_mask.shape[0] % (264*243) == 0:
-    #    shape = (int(DS.poc_mask.shape[0]/(264*243)),264,243)
-    #elif DS.poc_mask.shape[0] % (263*243) == 0:
-    #    shape = (int(DS.poc_mask.shape[0]/(263*243)),263,243)
-    #elif DS.poc_mask.shape[0] % (265*243) == 0:
-    #    shape = (int(DS.poc_mask.shape[0]/(265*243)),265,243)
-    ## TODO add 63909 shape...
-#    elif DS.poc_mask.shape[0] % (265*243) == 0:
-#        shape = (int(DS.poc_mask.shape[0]/(265*243)),265,243)
-    #else:
-    #    print("Weird shape modis data: {}".format(DS.poc_mask.shape[0]))
-    #    print(file)
-    #    print(DS)
-    #    return None
+    graphs = []
+    for j in range(label_no-1):
+        mask = (labels == j+1).astype('uint8')
+        # Process all dataset variables (including coordinates)
+        graphs.append(striped_boundary_data(mask, stripe_width, n_stripes, DS.variabes))
 
-    #print(shape)
-
-    #extract numpy arrays into dict
-    for variable in DS:
-        if variable == 'time':
-            continue
-        #try: 
-        data[variable] = DS[variable].values #.reshape(shape)
-        #except:
-        #   print('Something went wrong in the reshaping!')
-        #   return None
-
-    #Discard images with no pocs
-    poc_index = data['poc_mask'].any()
-    poc_only_data = {}
-    for variable, array in data.items():
-        poc_only_data[variable] = array[poc_index,...]
-        
-    del(data, DS)
-    poc_no = []
-    
-    graphs=[]
-    for i in range(poc_only_data['poc_mask'].shape[0]):
-        single_image_data={}
-        for variable, array in poc_only_data.items():
-            if variable == 'poc_mask':
-                single_image_data[variable] = array[i,...]>127
-            else:
-                single_image_data[variable] = array[i,...]
-        label_no, labels = cv2.connectedComponents((poc_only_data['poc_mask'][i,:,:]>127).astype('uint8'))
-        poc_no.append(label_no-1)
-        for j in range(label_no-1):
-            mask = (labels == j+1).astype('uint8')
-            graphs.append(striped_boundary_data(mask, 2, 40, single_image_data))
+    # Currently graph is a list of dicts. This turns that into a dict of lists...
     final_results = {}
     for variable, array in poc_only_data.items():
         final_results[variable] = []
@@ -170,20 +75,31 @@ def data_extract(file):
             final_results[variable].append(array)
             
     return final_results
-            
-    
+
     
 if __name__ == '__main__':
-    pool = Pool(num_procs)
-    inputs = [file for file in datagen(fileload,filelist)]
-    output = pool.map(data_extract, inputs)
-    #print(output)
-    pool.close()
-    pool.join()
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('files', nargs='*', help="Input file(s)")
+    parser.add_argument('-o', '--outfile', help="Output name", default='poc')
+    parser.add_argument('--overwrite', help="Output name", action='store_true')
+    parser.add_argument('-n', '--processes', help="Number of processes to run", default=1, type=int)
+    parser.add_argument('--stripes', help="Number of stripes around each POC", default=40, type=int)
+    parser.add_argument('--stripe-width', help="Pixel width of stripe", default=1, type=int)
 
-    if not os.path.exists('results'):
-        os.makedirs('results')
-    
-    print('Saving...')
-    with open('results/results_amsre_rain.pickle', 'wb') as handle:
-        pickle.dump(output, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    # Gets command line args by default
+    args = parser.parse_args()
+    outfile = f"results_{args.outfile}.pickle"
+    if os.path.isfile(outfile) and not args.overwrite:
+        print(f"Output file: {outfile} already exists. Exiting")
+    else:
+        # Read each file across as many processes as we can
+        with Pool(args.processes) as p:
+            output = p.map(partial(data_extract,
+                                   stripe_width=args.stripe_width,
+                                   n_stripes=args.stripes),
+                           args.files)
+
+        print(f'Saving output file: {outfile}')
+        with open(outfile, 'wb') as f:
+            pickle.dump(output, f, protocol=pickle.HIGHEST_PROTOCOL)
